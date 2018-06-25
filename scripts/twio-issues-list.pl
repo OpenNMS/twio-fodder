6019d0051e30bd0f59d59c4e37a62aa6cf7d2037
Benjamin Reed <ranger@opennms.org>
2018-06-25 13:40:20 -0400

    honor a copy of the search request in /tmp if it exists

--- scripts/twio-issues-list.pl
+++ scripts/twio-issues-list.pl
@@ -3,9 +3,11 @@
 use Term::ReadKey;
 use LWP::UserAgent;
 use XML::LibXML;
+use File::Slurp;
 
 # Change this if the Jira URL changes
-my $jiraBaseUrl = "https://issues.opennms.org";
+my $jiraHost = "issues.opennms.org";
+my $jiraBaseUrl = "https://${jiraHost}";
 
 my $filter = 13303; # 7 days
 #my $filter = 13522; # 8 days
@@ -16,36 +18,43 @@ my $filter = 13303; # 7 days
 my $jiraLoginUrl = "${jiraBaseUrl}/login.jsp";
 my $jiraSearchUrl = "${jiraBaseUrl}/sr/jira.issueviews:searchrequest-xml/${filter}/SearchRequest-${filter}.xml?tempMax=1000";
 
-# Read our login credentials
-my $creds = {};
-print "Jira login\n----------\nUsername: ";
-chomp ($creds->{os_username} = <STDIN>);
-print "Password: ";
-ReadMode('noecho');
-chomp($creds->{os_password} = <STDIN>);
-ReadMode(0);        # back to normal
- 
-# Construct the user-agent HTTP client
-my $ua = LWP::UserAgent->new( requests_redirectable => [ "GET", "HEAD", "POST" ] );
-$ua->cookie_jar( {} );
-$ua->timeout(30);
- 
-# Do the login POST to Jira
-my $response = $ua->post("${jiraLoginUrl}", $creds);
-if ($response->is_success) {
-  print "Logged in to Jira\n";
+my $text;
+if (-e "/tmp/SearchRequest-${filter}.xml" ) {
+  $text = read_file( "/tmp/SearchRequest-${filter}.xml" );
 } else {
-  die "Jira login failed: " . $response->status_line;
-}
+  # Read our login credentials
+  my $creds = {};
+  print "Jira login\n----------\nUsername: ";
+  chomp ($creds->{os_username} = <STDIN>);
+  print "Password: ";
+  ReadMode('noecho');
+  chomp($creds->{os_password} = <STDIN>);
+  ReadMode(0);        # back to normal
+   
+  # Construct the user-agent HTTP client
+  my $ua = LWP::UserAgent->new( requests_redirectable => [ "GET", "HEAD", "POST" ] );
+  $ua->cookie_jar( {} );
+  $ua->timeout(30);
+   
+  # Do the login POST to Jira
+  my $response = $ua->post("${jiraLoginUrl}", $creds);
+  if ($response->is_success) {
+    print "Logged in to Jira\n";
+  } else {
+    die "Jira login failed: " . $response->status_line;
+  }
+  
+  # Do the search GET to Jira
+  $response = $ua->get("${jiraSearchUrl}");
+  if (! $response->is_success) {
+    die "Jira search failed: " . $response->status_line;
+  }
 
-# Do the search GET to Jira
-$response = $ua->get("${jiraSearchUrl}");
-if (! $response->is_success) {
-  die "Jira search failed: " . $response->status_line;
+  $text = $response->decoded_content;
 }
 
 # Parse the XML document returned by the Jira search
-my $dom = XML::LibXML->load_xml(string => $response->decoded_content);
+my $dom = XML::LibXML->load_xml(string => $text);
 
 # Crawl the DOM and generate the Markdown output
 # * [HZN-266](http://issues.opennms.org/browse/HZN-266): Migrate discovery to Hibernate
